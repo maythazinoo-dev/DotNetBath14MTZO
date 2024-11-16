9832678b299ba1b4dfc050d790256237e4132ccf
May Thazin Oo <May Thazin Oo@ENG-18>
2024-11-16 10:42:59 +0630

    Add project files.

---
+++ DotNetBath14MTZO.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+Console.WriteLine($"Hello {username}");
+
+Console.ReadLine();
