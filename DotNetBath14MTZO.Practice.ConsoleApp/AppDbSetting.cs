e6ec69b1d7446364f8b799b927621fee6b1f5c1a
May Thazin Oo <maythazinoo18817@gmail.com>
2024-12-04 23:27:38 +0630

    Feat:ConsoleApp using Dapper

---
+++ DotNetBath14MTZO.Practice.ConsoleApp/AppDbSetting.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBath14MTZO.Practice.ConsoleApp
+{
+    public static class AppDbSetting
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder { get; } = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "WalletDB",
+            UserID = "sa",
+            Password = "mtzoo@123",
+            TrustServerCertificate=true
+            
+
+        };
+    }
+}
+    
+
---
+++ DotNetBath14MTZO.Practice.ConsoleApp/DapperPractices/DapperPractice.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using DotNetBath14MTZO.Practice.ConsoleApp.DapperPractices.BlogDtos;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBath14MTZO.Practice.ConsoleApp.DapperPractices
+{
+    public class DapperPractice
+    {
+        private readonly string _connection = AppDbSetting.SqlConnectionStringBuilder.ConnectionString;
+        public void Read()
+        {
+            using IDbConnection connection = new SqlConnection(_connection);
+            string query = "select * from Tbl_Blog";
+
+            List<BlogDto>lst = connection.Query<BlogDto>(query).ToList();
+            foreach(BlogDto item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            }
+
+        }
+
+        public void Edit(string id)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connection);
+
+            string query = $@"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+  FROM [dbo].[Tbl_Blog] where BlogId='{id}'";
+
+            var item = dbConnection.Query<BlogDto>(query).FirstOrDefault();
+            if (item is null)
+            {
+                Console.WriteLine("Not Found Data");
+                return;
+               
+            }
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+        }
+
+        public void Create(string title,string author,string content)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connection);
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           ('{title}'
+           ,'{author}'
+           ,'{content}')";
+
+            int result = dbConnection.Execute(query);
+            var message = result > 0 ? "Saving is Successful" : "Saving is failed";
+            Console.WriteLine(message);
+
+        }
+
+        public void Update(string id, string title,string author,string content)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connection);
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] ='{title}'
+      ,[BlogAuthor] = '{author}'
+      ,[BlogContent] = '{content}'
+ WHERE BlogId = '{id}'";
+
+            int result = dbConnection.Execute(query);
+            var message = result > 0 ? "Updating Successful" : "Updating Failed";
+            Console.WriteLine(message);
+        }
+
+        public void Delete(string id)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connection);
+            string query = $"delete from tbl_blog where BlogID= '{id}'";
+
+            int result = dbConnection.Execute(query);
+            var message = result > 0 ? "Delete is successed" : "Delete is failed";
+            Console.WriteLine(message);
+        }
+     
+    }
+}
