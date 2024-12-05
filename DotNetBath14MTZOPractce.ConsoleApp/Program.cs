c69cc69f8b62108764fad3627eed512690266541
May Thazin Oo <maythazinoo18817@gmail.com>
2024-12-05 12:27:25 +0630

    Feat: ConsoleApp using Efcore

---
+++ DotNetBath14MTZOPractce.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetBath14MTZO.Practice.ConsoleApp.DapperPractices;
+using DotNetBath14MTZOPractce.ConsoleApp.AdoDotNetPractice;
+using DotNetBath14MTZOPractce.ConsoleApp.EFCorePractices;
+
+//Console.WriteLine("Hello, World!");
+//AdoDotNetPractice adoDotNetPractice = new AdoDotNetPractice();
+//adoDotNetPractice.Read();
+//adoDotNetPractice.Edit("14F1959E-F700-42F4-98A1-5926F3804EE2");
+//adoDotNetPractice.Create("Blog Tiltle 10", "Blog Author 10", "Blog Content 10");
+//adoDotNetPractice.Update("95856D59-41A4-4F62-8CA4-9DCE62955813", "Blog Title 11", "Blog Author 11", "Blog Content 11");
+//adoDotNetPractice.Delete("95856D59-41A4-4F62-8CA4-9DCE62955813");
+
+
+DapperPractice dapperPractice = new DapperPractice();
+//dapperPractice.Read();
+//dapperPractice.Edit("AC9CB7FF-5792-47BA-A561-B289610AB182");
+//dapperPractice.Create("Test","test","test");
+//dapperPractice.Update("AC9CB7FF-5792-47BA-A561-B289610AB182", "Blog Title 5", "Blog Author 5", "Blog Conten 5");
+//dapperPractice.Delete("");
+
+EFCorePractice eFCorePractice = new EFCorePractice();
+//eFCorePractice.Read();
+//eFCorePractice.Edit("AC9CB7FF-5792-47BA-A561-B289610AB182");
+//eFCorePractice.create("testing", "testing", "testring");
+eFCorePractice.Update("937B377E-6CF8-4915-8F57-1A539181E55A", "Blog Title 6", "Blog Author 6", "Blog Conten 6");
+//eFCorePractice.Delete("");
