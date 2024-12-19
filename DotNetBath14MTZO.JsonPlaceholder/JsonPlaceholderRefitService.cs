666d3e5811674ea733c6800ddff95d1c2c36298d
May Thazin Oo <maythazinoo18817@gmail.com>
2024-12-20 06:21:12 +0630

    Feat : JsonPlaceHolder

---
+++ DotNetBath14MTZO.JsonPlaceholder/JsonPlaceholderRefitService.cs
@@ -0,0 +1,72 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBath14MTZO.JsonPlaceholder
+{
+    public class JsonPlaceholderRefitService
+    {
+        private readonly IJsonPlaceHolderApi _Placeholder;
+      
+        public JsonPlaceholderRefitService() 
+        {
+            _Placeholder = RestService.For<IJsonPlaceHolderApi>("https://jsonplaceholder.typicode.com")!;
+        }
+
+        public async Task<List<JsonModel>> GetPosts()
+        {
+
+            var model = await _Placeholder.GetPosts();
+            return model;
+
+        }
+
+
+        public async Task<JsonModel> GetPost(int id)
+        {
+            var model = await _Placeholder.GetPost(id);
+            return model;
+        }
+
+        public async Task<JsonModel> CreatePost(JsonModel requestModel)
+        {
+            var model = await _Placeholder.CreatePost(requestModel);
+            return model;
+        }
+
+        public async Task<JsonModel> UpdatePost(int id, JsonModel requestModel)
+        {
+            var modle = await _Placeholder.UpdatePost(id, requestModel);
+            return modle;
+        }
+
+        public async Task<JsonModel> DeletePost(int id)
+        {
+            var model = await _Placeholder.DeletePost(id);
+            return model;
+        }
+    }
+
+
+
+    public interface IJsonPlaceHolderApi
+    {
+        [Get("/Post")]
+        Task<List<JsonModel>> GetPosts();
+
+        [Get("/posts/{id}")]
+        public Task<JsonModel> GetPost(int id);
+
+        [Post("/posts")]
+        public Task<JsonModel> CreatePost(JsonModel requestModel);
+
+        [Patch("/posts/{id}")]
+        public Task<JsonModel> UpdatePost(int id, JsonModel requestModel);
+
+        [Delete("/posts/{id}")]
+        public Task<JsonModel> DeletePost(int id);
+    }
+}
---
+++ DotNetBath14MTZO.JsonPlaceholder/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetBath14MTZO.JsonPlaceholder;
+
+Console.WriteLine("Hello, World!");
+
+JsonPlaceholderRefitService jsonPlaceholderService = new JsonPlaceholderRefitService();
+try
+{
+    var jsonLists = await jsonPlaceholderService.GetPosts();
+
+    foreach (var item in jsonLists)
+    {
+        Console.WriteLine($"ID: {item.Id}, Title: {item.Title}, Body: {item.Body}");
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+Console.ReadLine();
+
+var post = await jsonPlaceholderService.GetPost(1);
+Console.WriteLine(post.Title);
+Console.ReadLine();
+
+
+var createPost= await jsonPlaceholderService.CreatePost(new JsonModel
+{
+    UserId = 100,
+    Title = "Refit Json Testing Title",
+    Body = "Refit Json Testing Body"
+});
+
+Console.WriteLine(createPost.UserId);
+Console.WriteLine(createPost.Title);
+Console.WriteLine(createPost.Body);
+Console.ReadLine();
+
+
+JsonModel updatePostModel = new JsonModel();
+
+updatePostModel.Id = 100;
+updatePostModel.Title = "Updating JsonPlace Holder Title using Refit";
+updatePostModel.Body = "Updating JsonPlace Holder Body using Refit";
+
+var updateResponse = await jsonPlaceholderService.UpdatePost(10, updatePostModel);
+Console.WriteLine(updateResponse.UserId);
+Console.WriteLine(updateResponse.Title);
+Console.WriteLine(updateResponse.Body);
+Console.ReadLine();
+
+
+var deletePost = await jsonPlaceholderService.DeletePost(10);
+Console.WriteLine(deletePost.Title);
+Console.ReadLine();
